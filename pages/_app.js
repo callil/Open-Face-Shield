e26ccae794d1912b7a3ebb9ad9d157c2ed406906
Callil Capuozzo <callil.capuozzo@gmail.com>
2020-04-21 08:55:50 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,12 @@ import '../styles/index.css'
 import Nav from '../components/nav'
 import Header from '../components/header'
 import Footer from '../components/footer'
+import Banner from '../components/banner'
 
 function MyApp({ Component, pageProps }) {
   return (
     <div className="md:flex flex-col items-center w-full">
+      <Banner />
       <Header />
       <Nav />
       <div className="max-w-xl pb-20 px-8 mx-auto">
